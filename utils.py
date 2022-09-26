e9566b64f3bb79eb467c692efdc1e3cb9c204745
marcderbauer <marcanthony.bauer@gmail.com>
2022-09-26 16:39:03 +0200

    cleaned up utils.py

--- utils.py
+++ utils.py
@@ -1,37 +1,29 @@
 from __future__ import unicode_literals, print_function, division
 from io import open
-import glob
-import os
 import unicodedata
 import string
 from random import shuffle
-import time
-import math
 
-# Currently declared twice, find a way around this
+
+# This was taken from a PyTorch tutorial iirc
 all_letters = string.ascii_letters + " .,;'-"
 
-def findFiles(path): return glob.glob(path)
-
-# Turn a Unicode string to plain ASCII, thanks to https://stackoverflow.com/a/518232/2809427
 def unicodeToAscii(s):
+    """
+    Turn a Unicode string to plain ASCII, thanks to https://stackoverflow.com/a/518232/2809427
+    """
     return ''.join(
         c for c in unicodedata.normalize('NFD', s)
         if unicodedata.category(c) != 'Mn'
         and c in all_letters
     )
 
-# Read a file and split into lines
 def readLines(filename):
+    """
+    Read a file and split into lines
+    """
     with open(filename, encoding='utf-8') as some_file:
         return [unicodeToAscii(line.strip()) for line in some_file]
-
-def timeSince(since):
-    now = time.time()
-    s = now - since
-    m = math.floor(s / 60)
-    s -= m * 60
-    return '%dm %ds' % (m, s)
 
 def save_file(filename, list):
     """
@@ -42,7 +34,6 @@ def save_file(filename, list):
                 f.write(f"{line}\n")
 
 def split_dset(dset, split, shuffle_dset=True):
-    # TODO: Included in data class now, can be removed soon.
     """
     Takes percentage that each split is supposed to be at and splits the dataset accodringly.
     input percentages must add to one
