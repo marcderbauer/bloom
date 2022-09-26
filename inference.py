93984def72c1513dee29e15ad1f6b50e8188c784
marcderbauer <marcanthony.bauer@gmail.com>
2022-09-26 11:50:12 +0200

    added argparse to inference and query_api.py

--- inference.py
+++ inference.py
@@ -1,112 +1,53 @@
 from transformers import BloomTokenizerFast, BloomForCausalLM
 import torch as pt
+import argparse
 
-# pt.manual_seed(0)
+#----------------------------------------------------------------------------
+#                               ARGPARSE
+#----------------------------------------------------------------------------
+parser = argparse.ArgumentParser(description='Generate inference for a given prompt')
+parser.add_argument("prompt", type=str, nargs="+", help="The prompt to generate inference from.")
+parser.add_argument("--temp", type=float, default=0.7, help="Temperature for inference (0-1). Lower is more chaotic.")
+parser.add_argument("--top_p", type=float, default=0.92)
+parser.add_argument("--top_k", type=int, default=50)
+parser.add_argument("--rp", type=float, default=1.2, help="Repetition penalty")
+parser.add_argument("--max_length", type=int, default=30)
+parser.add_argument("--min_length", type=int, default=None)
+parser.add_argument("--seed", type=int, default=None)
+args = parser.parse_args()
+
+if args.min_length == None:
+    args.min_length = len(args.prompt) + 1
+if args.seed:
+    pt.manual_seed(args.seed)
+assert args.max_length > args.min_length
+
+#----------------------------------------------------------------------------
+#                               MODEL
+#----------------------------------------------------------------------------
 
 tokenizer = BloomTokenizerFast.from_pretrained("bigscience/bloom-560m")
-model = BloomForCausalLM.from_pretrained("models/")
+model = BloomForCausalLM.from_pretrained("upload") #TODO: make this one consistent with output dir of main
 
-# inputs = tokenizer("Hello, my dog is cute", return_tensors="pt")
-# outputs = model(**inputs, labels=inputs["input_ids"])
-# loss = outputs.loss
-# logits = outputs.logits
-
-# print(logits)
-
+#----------------------------------------------------------------------------
+#                               INFERENCE
+#----------------------------------------------------------------------------
 
 # generate
-input_ids = tokenizer.encode('North Korean', return_tensors='pt')
-"""
+input_ids = tokenizer.encode(" ".join(args.prompt), return_tensors='pt')
 
-# activate beam search and early_stopping
-beam_output = model.generate(
-    input_ids, 
-    max_length=50, 
-    num_beams=5, 
-    no_repeat_ngram_size=2,
-    early_stopping=True
-)
-
-print("\nBeam output:\n" + 100 * '-')
-print(tokenizer.decode(beam_output[0], skip_special_tokens=True))
-
-
-# activate sampling and deactivate top_k by setting top_k sampling to 0
+# Top-p sampling
+# More info here: https://huggingface.co/blog/how-to-generate
 sample_output = model.generate(
     input_ids, 
     do_sample=True, 
-    max_length=50, 
-    top_k=0
+    max_length=args.max_length, 
+    min_length=args.min_length,
+    top_p=args.top_p, 
+    top_k=args.top_k,
+    temperature=args.temp,
+    repetition_penalty=args.rp
 )
 
-print("\nSample output:\n" + 100 * '-')
-print(tokenizer.decode(sample_output[0], skip_special_tokens=True))
-
-
-# adding temperature to the sample output
-sample_output = model.generate(
-    input_ids, 
-    do_sample=True, 
-    max_length=50, 
-    top_k=0,
-    temperature=0.7
-)
-
-print("\nSample output with Temperature = 0.7:\n" + 100 * '-')
-print(tokenizer.decode(sample_output[0], skip_special_tokens=True))
-
-
-# Top K sampling
-sample_output = model.generate(
-    input_ids, 
-    do_sample=True, 
-    max_length=50, 
-    top_k=50
-)
-
-print("\nTop k=50 sampling:\n" + 100 * '-')
-print(tokenizer.decode(sample_output[0], skip_special_tokens=True))
-
-
-
-# Top-p sampling -- deactivate top_k sampling and sample only from 92% most likely words
-sample_output = model.generate(
-    input_ids, 
-    do_sample=True, 
-    max_length=50, 
-    top_p=0.92, 
-    top_k=0
-)
-
-print("\nTop P sampling output:\n" + 100 * '-')
-print(tokenizer.decode(sample_output[0], skip_special_tokens=True))
-
-
-# Top-p sampling -- deactivate top_k sampling and sample only from 92% most likely words
-sample_output = model.generate(
-    input_ids, 
-    do_sample=True, 
-    max_length=50, 
-    top_p=0.92, 
-    top_k=0,
-    temperature=0.7
-)
-
-print("\nTop with temp = 0.7:\n" + 100 * '-')
-print(tokenizer.decode(sample_output[0], skip_special_tokens=True))
-
-"""
-
-# Top-p sampling -- deactivate top_k sampling and sample only from 92% most likely words
-sample_output = model.generate(
-    input_ids, 
-    do_sample=True, 
-    max_length=20, 
-    top_p=0.92, 
-    top_k=50,
-    temperature=0.7,
-    repetition_penalty=1.2
-)
-
-print("\nTop with temp=0.7; k=50, p=0.92,rep=1.2:\n" + 100 * '-')
+print(f"\nTop with temp={args.temp}; k={args.top_k}, p={args.top_p}, rep={args.rp}:\n" + 100 * '-')
 print(tokenizer.decode(sample_output[0], skip_special_tokens=True))
--- youtube/query_api.py
+++ youtube/query_api.py
@@ -9,19 +9,31 @@ import os
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
-import json
+import argparse
+
+#----------------------------------------------------------------------------
+#                               ARGPARSE
+#----------------------------------------------------------------------------
+parser = argparse.ArgumentParser(description='Generate inference for a given prompt')
+parser.add_argument("playlist_ids", type=str, nargs="?", default="UUn8zNIfYAQNdrFRrr8oibKw PLw613M86o5o7q1cjb26MfCgdxJtshvRZ-", help="The ids of the playlists to download")
+parser.add_argument("--verbose", default=False, action='store_true')
+args = parser.parse_args()
 
 scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
 RESULTS_PER_PAGE = 50
 
+
+#----------------------------------------------------------------------------
+#                               MAIN
+#----------------------------------------------------------------------------
 def main():
     # Disable OAuthlib's HTTPS verification when running locally.
     # *DO NOT* leave this option enabled in production.
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "0"
 
     api_service_name = "youtube"
     api_version = "v3"
-    client_secrets_file = "YOUR_CLIENT_SECRET_FILE.json"
+    client_secrets_file = "client_secret_900137197244-2trfkrsu9h0a57oi51dcgkg30mbetlha.apps.googleusercontent.com.json"
 
     # Get credentials and create an API client
     flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
@@ -31,31 +43,50 @@ def main():
         api_service_name, api_version, credentials=credentials)
 
     all_titles = []
-    nextPageToken = None
-    
-    while True:
-        request = youtube.playlistItems().list(
-            part="snippet",
-            maxResults=50,
-            pageToken = nextPageToken,
-            playlistId="UUn8zNIfYAQNdrFRrr8oibKw"
-        )
-        response = request.execute()
-        try:
-            nextPageToken = response['nextPageToken']
 
+    for playlist_id in args.playlist_ids.split():
+        channel_titles = []
+        nextPageToken = None
+        channel= None
+
+        while True:
+            request = youtube.playlistItems().list(
+                part="snippet",
+                maxResults=50,
+                pageToken = nextPageToken,
+                playlistId=playlist_id
+            )
+            response = request.execute()
+            try:
+                nextPageToken = response['nextPageToken']
+            except:
+                nextPageToken = None
+
+            if not channel:
+                channel = response['items'][0]['snippet']['channelTitle']
             for item in response['items']:
                 title = item['snippet']['title']
-                all_titles.append(title)
-                print(title)
+                channel_titles.append(title)
+                if args.verbose:
+                    print(title)
 
-        except:
-            print(len(response['items']))
-            break
+            if not nextPageToken:
+                all_titles.extend(channel_titles)
+                break
 
-    with open("data/titles.txt", "w") as f:
-            for title in all_titles:
-                f.write(f"{title}\n")
+        if not channel:
+            channel = "titles"
+            
+        with open(f"data/{channel}.txt", "w") as f:
+                for title in channel_titles:
+                    f.write(f"{title}\n")
+        print(f"\n{'-'*100}\nFinished downloading {channel}. Total items: {len(channel_titles)}\n")
+    
+    # Save all titles if multiple playlists were given
+    if len(args.playlist_ids.split()) > 1:
+        with open(f"data/combined.txt", "w") as f:
+                for title in all_titles:
+                    f.write(f"{title}\n")
 
 if __name__ == "__main__":
     main()
